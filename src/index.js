459ea3e17c02272102f648fbded92cdb16ccb694
Brian Eng <brian@brianeng.com>
2016-10-21 16:41:32 -0500

    nope

--- src/index.js
+++ src/index.js
@@ -6,24 +6,18 @@ var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var Die = React.createClass({
-  render: function() {
-    return (
-      <div className="col-sm-4">
-        <img src={"http://golearntocode.com/images/dice/" + this.props.number + ".png"} />
-      </div>
-    )
-  }
-})
-
 var App = React.createClass({
   render: function() {
     return (
       <div className="container">
         <h1>Welcome to the KIEI-924 Casino!</h1>
         <div className="row">
-          <Die number={getRandomInt(1, 6)} />
-          <Die number={getRandomInt(1, 6)} />
+          <div className="col-sm-4">
+            <img src={"http://golearntocode.com/images/dice/3.png"} />
+          </div>
+          <div className="col-sm-4">
+            <img src={"http://golearntocode.com/images/dice/4.png"} />
+          </div>
         </div>
       </div>
     )
